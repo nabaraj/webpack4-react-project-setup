90114a07f6b6f21148e5a0aea315b413d07058a7
ine12362324 <nabaraj.saha@tesco.com>
2019-06-19 15:44:19 +0530

    basic page

--- src/component/Grid.js
+++ src/component/Grid.js
@@ -8,34 +8,12 @@ class Grid extends Component {
     return (
       <div>
         <div className="row">
-          <div className="col s2 purple lighten-5">1</div>
-          <div className="col s2 purple lighten-4">2</div>
-          <div className="col s2 purple lighten-3">3</div>
-          <div className="col s2 purple lighten-2">4</div>
-          <div className="col s2 purple lighten-1">5</div>
-          <div className="col purple s2">6</div>
-        </div>
-        <div class="row pink lighten-3">
-          <div class="col s12">
-            This div is 12-columns wide on all screen sizes
-          </div>
-          <div class="col s6">6-columns (one-half)</div>
-          <div class="col s6">6-columns (one-half)</div>
-        </div>
-        <div class="col s12 m2">
-          <p class="z-depth-1">z-depth-1</p>
-        </div>
-        <div class="col s12 m2">
-          <p class="z-depth-2">z-depth-2</p>
-        </div>
-        <div class="col s12 m2">
-          <p class="z-depth-3">z-depth-3</p>
-        </div>
-        <div class="col s12 m2">
-          <p class="z-depth-4">z-depth-4</p>
-        </div>
-        <div class="col s12 m2">
-          <p class="z-depth-5">z-depth-5</p>
+          <div className="col s2 cyan lighten-5">1</div>
+          <div className="col s2 cyan lighten-4">2</div>
+          <div className="col s2 cyan lighten-3">3</div>
+          <div className="col s2 cyan lighten-2">4</div>
+          <div className="col s2 cyan lighten-1">5</div>
+          <div className="col cyan s2">6</div>
         </div>
       </div>
     );
